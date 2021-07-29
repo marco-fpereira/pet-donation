813876adb7b261a88d64594bc24bb3559dd88121
Marco Antônio <marcoantonio_fp@usp.br>
2021-07-29 02:15:26 -0300

    adição de home do responsavel

--- src/main/java/com/each/bd2/PetDonation/controller/ResponsavelController.java
+++ src/main/java/com/each/bd2/PetDonation/controller/ResponsavelController.java
@@ -1,11 +1,13 @@
 package com.each.bd2.PetDonation.controller;
 
 import com.each.bd2.PetDonation.entities.Responsavel;
+import com.each.bd2.PetDonation.service.PetService;
 import com.each.bd2.PetDonation.service.ResponsavelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -17,6 +19,9 @@ public class ResponsavelController {
     @Autowired
     ResponsavelService responsavelService;
 
+    @Autowired
+    PetService petService;
+
     @GetMapping("home")
     public String home(Model model, Principal principal){
         Responsavel responsavel = responsavelService.findByUsername(principal.getName());
@@ -25,8 +30,9 @@ public class ResponsavelController {
         return "usuario/responsavel/homeresponsavel";
     }
 
-    @GetMapping("aceiteadocao")
-    public String aceiteAdocao(){
+    @GetMapping("aceiteadocao/{id}")
+    public String aceiteAdocao(@PathVariable(value = "id", required = false) Long pet_id, Model model){
+        model.addAttribute("pet", petService.findById(pet_id));
         return "usuario/responsavel/aceiteadocao";
     }
 
